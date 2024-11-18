dea9b7f42069983f6ceaaaa8f324b642724ffee2
Matthew Lewis <matthew.a.lewis7@gmail.com>
2024-11-18 12:09:00 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
               <Link href="/">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  fill="#ffffff"
+                  fill="currentColor"
                   viewBox="0 0 284 65"
                   className="min-h-5"
                 >
